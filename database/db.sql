a5dd319187aa0ad007d815bbff29023e758c3c2d
unknown <angga@gmail.com>
2023-12-28 11:37:27 +0700

    pulsa done database

--- database/db.sql
+++ database/db.sql
@@ -1,6 +1,6 @@
 /*
-SQLyog Enterprise v12.5.1 (64 bit)
-MySQL - 10.4.28-MariaDB : Database - dbtopup
+SQLyog Ultimate v13.1.1 (64 bit)
+MySQL - 10.1.37-MariaDB : Database - dbtopup
 *********************************************************************
 */
 
@@ -12,7 +12,7 @@ MySQL - 10.4.28-MariaDB : Database - dbtopup
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`dbtopup` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`dbtopup` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
 USE `dbtopup`;
 
@@ -28,9 +28,13 @@ CREATE TABLE `paket` (
   `jumlah_telpon` int(11) DEFAULT NULL,
   `masa_aktif` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id_paket`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `paket` */
+
+insert  into `paket`(`id_paket`,`nama_paket`,`harga_paket`,`jumlah_data`,`jumlah_telpon`,`masa_aktif`) values 
+(1,'Loop Internet 1 GB',14000,'1 GB',30,'30 Hari'),
+(2,'Combo 6,5GB',63000,'Internet 4,5GB,Vidiomax 2GB',100,'30 Hari');
 
 /*Table structure for table `pembeli` */
 
@@ -40,7 +44,7 @@ CREATE TABLE `pembeli` (
   `id_pembeli` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(60) DEFAULT NULL,
   PRIMARY KEY (`id_pembeli`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `pembeli` */
 
@@ -54,9 +58,15 @@ CREATE TABLE `pulsa` (
   `provider` varchar(50) DEFAULT NULL,
   `harga_pulsa` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_pulsa`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `pulsa` */
+
+insert  into `pulsa`(`id_pulsa`,`jumlah_pulsa`,`provider`,`harga_pulsa`) values 
+(1,5000,'Telkomsel',7000),
+(2,10000,'Telkomsel',12000),
+(3,15000,'Telkomsel',17000),
+(4,20000,'Telkomsel',23000);
 
 /*Table structure for table `transaksi` */
 
@@ -67,7 +77,7 @@ CREATE TABLE `transaksi` (
   `id_pembeli` int(11) DEFAULT NULL,
   `id_paket` int(11) DEFAULT NULL,
   `id_pulsa` int(11) DEFAULT NULL,
-  `waktu` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `waktu` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `jenis` enum('paket','pulsa') DEFAULT NULL,
   `detail` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_transaksi`),
@@ -77,7 +87,7 @@ CREATE TABLE `transaksi` (
   CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_pembeli`) REFERENCES `pembeli` (`id_pembeli`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`id_paket`) REFERENCES `paket` (`id_paket`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `transaksi_ibfk_3` FOREIGN KEY (`id_pulsa`) REFERENCES `pulsa` (`id_pulsa`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `transaksi` */
 
